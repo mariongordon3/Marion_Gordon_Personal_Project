8da9f5ba08e36db47fc1e1facf2e03a4119b8572
Marion Gordon <mariongordon3@yahoo.com>
2023-08-11 11:56:22 -0500

    started plate_app models and finished user_app models

--- plate_app/models.py
+++ plate_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from user_app.models import User
 # Create your models here.
+class Measurements(models.Model):
+    amount = models.PositiveBigIntegerField()
+    unitName = models.CharField()
+
+class Nutrient(models.Model):
+    name = models.CharField()
+    measurement_id = models.ForeignKey(Measurements, ondelete=models.CASCADE, related_name='measurements')
+    is_macro = models.BooleanField(default=False)
+# class Plate(models.Model):
